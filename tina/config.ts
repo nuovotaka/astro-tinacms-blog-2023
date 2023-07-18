2bd179d899144176835d6a0ac8c583a4268e2c5f
tak n <tkn.infomail@gmail.com>
2023-07-18 16:47:29 +0900

    fix get tinacms config

--- tina/config.ts
+++ tina/config.ts
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: import.meta.env.TINA_CLIENT_ID, // Get this from tina.io
-  token: import.meta.env.TINA_CLIENT_TOKEN, // Get this from tina.io
+  clientId: process.env.TINA_CLIENT_ID, // Get this from tina.io
+  token: process.env.TINA_CLIENT_TOKEN, // Get this from tina.io
 
   build: {
     outputFolder: "admin",
